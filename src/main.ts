7c120848a6bedea91c78159bf19655dd7a889514
Benjamin Zaporzan <benzaporzan@gmail.com>
2025-12-03 17:03:03 -0500

    More work on converting to new scripting system

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { Instance as CSS } from "cs_script/point_script";
-import * as SE from "scriptedeuch";
+import { Vector, QAngle } from "scriptedeuch";
 
 CSS.OnActivate(() => {
     CSS.Msg("BundledEuch has been Activated!");
 });
 
 CSS.OnScriptReload({after:() => {
-    CSS.Msg("BundledEuch has been Reloaded!");
+    let v = Vector.create(0, 1, 2);
+    CSS.Msg("Vector: " + JSON.stringify(v));
+    CSS.Msg("BundledEuch has been Reloaded!!");
 }});
-
